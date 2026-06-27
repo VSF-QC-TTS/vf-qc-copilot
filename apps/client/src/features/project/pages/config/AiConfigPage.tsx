555c20c228ffcc28a7f5dd33f614dec7f49ad62e
Long <nghlong3004@gmail.com>
2026-06-27 22:48:05 +0700

    chore(ui): remove unused useEffect import in AiConfigPage

--- apps/client/src/features/project/pages/config/AiConfigPage.tsx
+++ apps/client/src/features/project/pages/config/AiConfigPage.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { motion } from 'motion/react'
 import { useTranslation } from 'react-i18next'
